8363ac76fa2c88a91f7be27d71f07f1f694cfaa9
OH <rudals2148@naver.com>
2024-06-24 03:41:52 +0900

    [feat] 타입 변경

--- app/(after_login)/_component/Main.tsx
+++ app/(after_login)/_component/Main.tsx
@@ -1,16 +1,19 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React, { ReactNode, useEffect } from "react";
 import { auth } from "@/firebase/firebase";
 import { onAuthStateChanged } from "firebase/auth";
 import BeforeLogin from "@/app/(before_login)/_component/BeforeMain";
 import SideBar from "@/components/Sidebar";
 import Container from "./Container";
 import useAuthValid from "@/store/authValid";
-import { ChildrenProps } from "./types";
 import LogoutButton from "./LogOut";
 
-const Main = ({ children }: ChildrenProps) => {
+interface MainProps {
+  children: ReactNode;
+}
+
+const Main: React.FC<MainProps> = ({ children }) => {
   const { isLogin, setIsLogin } = useAuthValid();
 
   useEffect(() => {
