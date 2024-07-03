ea20eecc2442a4b1edefc64c92edef9d2290cca6
OH <rudals2148@naver.com>
2024-07-03 23:22:48 +0900

    [feat] 상세 페이지 SSR 적용

--- app/(after_login)/post/[id]/page.tsx
+++ app/(after_login)/post/[id]/page.tsx
@@ -1,5 +1,25 @@
-import DetailModalContent from "../_component/DetailModalContent";
+import DetailModalContent from "@/app/(after_login)/post/_component/DetailModalContent";
+import { getPostById } from "@/utils/getPostDetail";
+import {
+  HydrationBoundary,
+  QueryClient,
+  dehydrate,
+} from "@tanstack/react-query";
 
-export default function Page() {
-  return <DetailModalContent />;
+type Props = { params: { id: string } };
+
+export default async function Page({ params }: Props) {
+  const queryClient = new QueryClient();
+  const postId = params.id;
+
+  await queryClient.prefetchQuery({
+    queryKey: ["feeds", postId],
+    queryFn: getPostById,
+  });
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <DetailModalContent />
+    </HydrationBoundary>
+  );
 }
--- utils/getPostDetail.ts
+++ utils/getPostDetail.ts
@@ -4,10 +4,7 @@ import { db } from "@/firebase/firebase";
 export const getPostById = async ({ queryKey }: any) => {
   const postId = queryKey[1];
   const docRef = doc(db, "feeds", postId);
-  const docSnap = await getDoc(docRef);
-  if (docSnap.exists()) {
-    return docSnap.data();
-  } else {
-    throw new Error("존재하지 않는 게시글입니다.");
-  }
+  const res = await getDoc(docRef);
+
+  return res.data();
 };
