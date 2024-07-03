c907edcfe590336748ece762168847d05ca50678
OH <rudals2148@naver.com>
2024-07-03 18:41:11 +0900

    [feat] 상태관리

--- store/modal.ts
+++ store/modal.ts
@@ -2,15 +2,18 @@ import { create } from "zustand";
 
 interface ModalState {
   data?: null;
-  type: "menu" | "new" | "edit" | "detail";
-  setType: (type: "menu" | "new" | "edit" | "detail") => void;
+  type: "menu" | "new" | "edit" | "detail_menu" | "comment_menu";
+  setType: (
+    type: "menu" | "new" | "edit" | "detail_menu" | "comment_menu"
+  ) => void;
   isOpen: boolean;
   setIsOpen: (isOpen: boolean) => void;
 }
 
 const useModal = create<ModalState>((set) => ({
   type: "menu",
-  setType: (type: "menu" | "new" | "edit" | "detail") => set({ type }),
+  setType: (type: "menu" | "new" | "edit" | "detail_menu" | "comment_menu") =>
+    set({ type }),
   isOpen: false,
   setIsOpen: (isOpen: boolean) => set({ isOpen }),
 }));
