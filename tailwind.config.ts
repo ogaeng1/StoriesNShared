3ca014a143d85ece424067b8f3984a01c6efd907
OH <rudals2148@naver.com>
2024-07-03 18:44:51 +0900

    [chore] 플러그인 설치

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,13 +8,22 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      screens: {
+        mobile: "480px",
+        tablet: "768px",
+        laptop: "1024px",
+      },
+      keyframes: {
+        spinner: {
+          from: { transform: "rotate(0deg)" },
+          to: { transform: "rotate(360deg)" },
+        },
+      },
+      animation: {
+        spinner: "spinner 1.5s linear infinite",
       },
     },
   },
-  plugins: [],
+  plugins: [require("tailwind-scrollbar-hide")],
 };
 export default config;
