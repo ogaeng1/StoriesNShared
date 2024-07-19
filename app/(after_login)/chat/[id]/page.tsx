c30103785ebaddc381af2f48f40e4ae531979356
OH <rudals2148@naver.com>
2024-07-19 09:58:31 +0900

    [feat] SSR

--- app/(after_login)/chat/[id]/page.tsx
+++ app/(after_login)/chat/[id]/page.tsx
@@ -1,17 +1,26 @@
 import SideBar from "@/components/Sidebar";
 import Container from "../../_component/Container";
 import ChattingRoom from "../_component/ChattingRoom";
+import {
+  HydrationBoundary,
+  dehydrate,
+  QueryClient,
+} from "@tanstack/react-query";
 
-const ChatList = () => {
+const ChatList = async () => {
+  const queryClient = new QueryClient();
+
   return (
-    <div className="flex min-w-[438px] min-h-[100vh] h-full overflow-x-hidden">
-      <SideBar />
-      <div className="flex-1 flex items-center justify-center">
-        <Container>
-          <ChattingRoom />
-        </Container>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <div className="flex min-w-[438px] min-h-[100vh] h-full overflow-x-hidden">
+        <SideBar />
+        <div className="flex-1 flex items-center justify-center">
+          <Container>
+            <ChattingRoom />
+          </Container>
+        </div>
       </div>
-    </div>
+    </HydrationBoundary>
   );
 };
 
--- app/(after_login)/follow-feeds/[id]/page.tsx
+++ app/(after_login)/follow-feeds/[id]/page.tsx
@@ -1,17 +1,36 @@
 import SideBar from "@/components/Sidebar";
 import Container from "../../_component/Container";
 import CurLoginUser from "../_component/CurLoginUser";
+import {
+  HydrationBoundary,
+  dehydrate,
+  QueryClient,
+  useQuery,
+} from "@tanstack/react-query";
+import { getFollowingList } from "@/utils/getFollowList";
 
-const FollowFeedPage = () => {
+type Props = { params: { id: string } };
+
+const FollowFeedPage = async ({ params }: Props) => {
+  const queryClient = new QueryClient();
+  const id = params.id;
+
+  await queryClient.prefetchQuery({
+    queryKey: ["following", id],
+    queryFn: () => getFollowingList(id),
+  });
+
   return (
-    <div className="flex min-w-[438px] min-h-[100vh] h-full overflow-x-hidden">
-      <SideBar />
-      <div className="flex-1 flex items-center justify-center">
-        <Container>
-          <CurLoginUser />
-        </Container>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <div className="flex min-w-[438px] min-h-[100vh] h-full overflow-x-hidden">
+        <SideBar />
+        <div className="flex-1 flex items-center justify-center">
+          <Container>
+            <CurLoginUser />
+          </Container>
+        </div>
       </div>
-    </div>
+    </HydrationBoundary>
   );
 };
 
