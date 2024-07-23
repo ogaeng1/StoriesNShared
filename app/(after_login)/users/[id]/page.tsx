9906bbdb3b029b40b775115163bc3d22ccb5aa47
OH <rudals2148@naver.com>
2024-07-24 00:37:26 +0900

    [fix] 불필요 선언 제거

--- app/(after_login)/users/[id]/page.tsx
+++ app/(after_login)/users/[id]/page.tsx
@@ -1,6 +1,4 @@
-import { Suspense } from "react";
 import UserExistValid from "../_component/UserExistValid";
-import Loading from "../../loading";
 
 const MyPage = () => {
   return <UserExistValid />;
